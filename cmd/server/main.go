04d3899c915521d74289de0df5310c4fab9b597d
Huihuang Yu <huihuang.yu@gmail.com>
2023-02-17 15:58:58 +1100

    added api/health entry pointer

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/huihuangyu/go-rest-api-gorm-fiber/internal/transport/http"
+)
 
 // App - the struct which contains things like the pointers to database connections
 type App struct {
@@ -8,6 +13,15 @@ type App struct {
 
 func (app *App) Run() error {
 	fmt.Println("Starting up our application.")
+
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
+
 	return nil
 }
 
